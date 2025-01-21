89bd41dc2ae87ea20e53e937011903b233c820fb
CORP\muhammadaw.zulifqar <muhammadawais.zulifqar@ibex.co>
2025-01-21 16:09:32 +0500

    no message

--- TeamCollab/settings.py
+++ TeamCollab/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$&t*h%l6o=w1u1&)7w1fy40&*)o6-3y_x(o1hsv5fqwjjk*%k%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.30.11.228', 'testbed-voice.enteract.cloud']
+ALLOWED_HOSTS = ['10.30.11.228', 'testbed-voice.enteract.cloud','127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
      # 'api',
-    'reports'
+    'reports',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -62,6 +63,7 @@ SWAGGER_SETTINGS = {
 # AUTH_USER_MODEL = 'api.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -171,3 +173,30 @@ MEDIA_ROOT = '/var/www/html/console_telephony/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS= True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000", 
+] 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.example\.com$",
+]
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
